28bd7d654567e931a796de4fb7993b4c15d155b0
Sumit Sahrawat <sumitsahrawat@outlook.com>
2014-07-13 01:50:35 +0530

    Refactoring and breaking code into functions

--- quotes.py
+++ quotes.py
@@ -14,39 +14,78 @@ Generates random quote
 
 import requests
 
-def main():
+def getQuote():
     """
-    Requests a random quote using forismatic API
+    Gets a random quote using forismatic API
     """
+    # The getQuote function's work begins after these function definitions
 
-    # Specification for the request
-    quote = {'method': 'getQuote', 'format': 'json', 'lang':'en'}
+    def requestQuote():
+        """
+        Requests a random quote from forismatic.
+        Returns a JSON string
+        """
+        
+        # Specification for the request
+        quote = {'method': 'getQuote', 'format': 'json', 'lang':'en'}
 
-    try:
         # Post Request
         r = requests.post("http://api.forismatic.com/api/1.0/", params = quote)
 
+        # Return data received
+        return r.text
+
+    def saveQuoteJSON(quote):
+        """
+        Takes a string and saves it to the quote.json file
+        """
         # Open file used for storing last seen quote
         f = open('quote.json', 'w')
 
         # Save newly fetched quote to file 
-        f.write(r.text)
+        f.write(quote)
 
         # Close file
         f.close()
-        
-        # Print Quote
-        print(r.text)
-        
-    except:
+    
+    def readQuoteJSON():
         # Open old quotes file for reading
-        f = open('quote', 'r')
+        f = open('quote.json', 'r')
 
         # Read file and return old contents
-        print(f.read())
+        quote = f.read()
 
         # Close file
         f.close()
+
+        return quote
+
+    def printQuote(quote):           # TODO: Actually parse JSON instead of just printing it out
+        print(quote)
+        
+    # The function's work begins here        
+    try:
+        # Request a quote
+        quote_json = requestQuote()
+        
+        # Save the quote to file
+        saveQuoteJSON(quote_json)
+        
+        # Print Quote
+        printQuote(quote_json)
+        
+    except:
+        # Read old quote from quote.json
+        quote_json = readQuoteJSON()
+
+        # Print Quote
+        printQuote(quote_json)
+        
+def main():
+    """
+    Requests a random quote using forismatic API
+    """
+    getQuote()
 
 if __name__ == '__main__':
     main()
