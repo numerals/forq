4c483643a98fbc6abddd8c5a2e8d45d2863d5f3d
Sumit Sahrawat <sumitsahrawat@outlook.com>
2014-07-18 18:54:20 +0530

    Commented install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,29 @@
 # Licensed under the MIT license.
 ##
 
+# The directory where the install.sh script is kept
 SCRIPT_DIR=$(readlink -f ${0%/*})
+
+# Whether root access is provided
 ROOT_ACCESS=1
+
+# UID of the user
 USER_UID=$($SCRIPT_DIR/conf/uid.sh)
 
+# If user = root, the root_access = true
 if [ "$USER_UID" = "0" ]
 then
     ROOT_ACCESS=0
 fi
 
+# If no root access, exit
 if [ "$ROOT_ACCESS" != "0" ]
 then
     echo 'Cannot install without root access'
     exit 1
 fi
 
+# Path to install
 path=/usr/local/forq
 
 if [ ! -d $path ]
@@ -30,6 +38,7 @@ then
     mkdir $path           # Make directory if directory does not exist
 fi
 
+# Install files
 install -m 0755 "$SCRIPT_DIR/src/generate.py" "$path"
 install -m 0644 "$SCRIPT_DIR/src/getQuote.py" "$path"
 install -m 0644 "$SCRIPT_DIR/src/quotes.json" "$path"
